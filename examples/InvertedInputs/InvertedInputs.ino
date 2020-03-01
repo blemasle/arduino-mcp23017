0fa87a0549e379ec77c5ca3a4e9eb2f5abb58033
Jörg Bernau <joerg@bernau.family>
2020-03-01 20:12:44 +0100

    Added comment to InvertedInputs.ino

--- examples/InvertedInputs/InvertedInputs.ino
+++ examples/InvertedInputs/InvertedInputs.ino
@@ -4,18 +4,26 @@
 #include <MCP23017.h>
 
 /**
+ * This sketch demonstrates the pin capabilities in pin- and portmode. 
+ * Each of the 16 bits can eigher  be an input which is inverted or not 
+ * or having a 100k pullup resistor in same mode or just beeing an output.
+ * 
+ * Some Interrupt capabilites are also shown
+ * 
  * Please connect:
- *    A5  => SCL   (ON UNO R3)
- *    A4  => SDA   (ON UNO R3)
- *    A3  => RST   (ON UNO R3)
- *    A0  =>  B0
- *    A1  =>  B1
- *    A2  =>  B2
- *    A3  =>  B3
- *    A4  =>  B4
- *    A5  =>  B5
- *    A6  =>  B6
- *    A7  =>  B7
+ *    A5   => SCL   (ON UNO R3)
+ *    A4   => SDA   (ON UNO R3)
+ *    A3   => RST   (ON UNO R3)
+ *    INTA =>  D2   (ON UNO R3)
+ *    INTB =>  D3   (ON UNO R3)
+ *    A0   =>  B0
+ *    A1   =>  B1
+ *    A2   =>  B2
+ *    A3   =>  B3
+ *    A4   =>  B4
+ *    A5   =>  B5
+ *    A6   =>  B6
+ *    A7   =>  B7
  */
 
 #define MCP23017_ADDR 0x20
